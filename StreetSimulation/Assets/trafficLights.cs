20055c67adea5903e8243fca3cb3744b81a9fc6f
David Vieyra <davidsvgd@hotmail.com>
2023-11-29 00:30:10 -0600

    Prefabs

---
+++ StreetSimulation/Assets/trafficLights.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class TrafficLightController : MonoBehaviour
+{
+    [SerializeField] private bool isGreen = false;
+    public GameObject greenLight;
+    public GameObject redLight;
+
+    public float lightChangeInterval = 5.0f; // Time interval for light change in seconds
+    private float timer;
+
+    void Start()
+    {
+        // Ensure the initial state of lights
+        ToggleLights();
+        timer = lightChangeInterval; // Start the timer
+    }
+
+    void Update()
+    {
+        ToggleLights();
+    }
+
+    void ToggleLights()
+    {
+        // Activate/deactivate lights based on the boolean value
+        if (isGreen)
+        {
+            greenLight.SetActive(true);
+            redLight.SetActive(false);
+        }
+        else
+        {
+            greenLight.SetActive(false);
+            redLight.SetActive(true);
+        }
+    }
+}
