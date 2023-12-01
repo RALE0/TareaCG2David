c369c40d4d06689401614d3482a31a3f023bd64e
David Vieyra <davidsvgd@hotmail.com>
2023-11-30 23:58:30 -0600

    Commentando código

--- trafficBase/V2/model.py
+++ trafficBase/V2/model.py
@@ -8,11 +8,11 @@ import requests
 
 class CityModel(Model):
     def __init__(self, numero_coches_max):
-        # Get the data dictionary from the json file.
+        # Cargar el diccionario de datos desde un archivo JSON.
         dataDictionary = json.load(open("trafficBase/V2/city_files/mapDictionary.json"))
         self.traffic_lights = []
 
-        # Load the map file. The map file is a text file where each character represents an agent.
+        # Cargar el archivo de mapa, donde cada carácter representa un agente.
         with open('trafficBase/V2/city_files/2023_base.txt') as baseFile:
             lines = baseFile.readlines()
             self.width = len(lines[0])-1
@@ -24,9 +24,10 @@ class CityModel(Model):
             self.I_locations = []
             self.D_locations = []
 
-            # Goes through each character in the map file and creates the corresponding agent.
+            # Crear agentes correspondientes para cada carácter en el archivo de mapa.
             for r, row in enumerate(lines):
                 for c, col in enumerate(row):
+                    # Crear agentes de carretera, semáforos, obstáculos, puntos de inicio y destino.
                     if col in ["v", "^", ">", "<"]:
                         agent = Road(f"r_{r*self.width+c}", self, dataDictionary[col])
                         self.grid.place_agent(agent, (c, self.height - r - 1))  
@@ -52,7 +53,7 @@ class CityModel(Model):
                         self.grid.place_agent(agent, (c, self.height - r - 1))
                         self.D_locations.append((c, self.height - r - 1))
         
-        
+        # Inicializar variables de control, como número de autos, contador de identificación, etc.
         self.num_cars = 0
         self.id_count = 0
         self.numero_coches_max = numero_coches_max
@@ -60,9 +61,10 @@ class CityModel(Model):
         self.step_count = 0
         self.used_I_locations = []
         self.arrived_cars = 0
-        self.initialize_car()
+        self.initializeCars()
 
-    def initialize_car(self):
+    def initializeCar(self):
+        # Inicializar un coche en una ubicación de inicio aleatoria si es posible.
         if self.I_locations and self.D_locations:
             random_I_location = random.choice(self.I_locations)
             random_D_location = random.choice(self.D_locations)
@@ -80,7 +82,8 @@ class CityModel(Model):
             self.id_count += 1
             print("NUMBER OF CARS", self.num_cars)
 
-    def initialize_cars(self):
+    def initializeCars(self):
+        # Inicializar coches en todas las ubicaciones de inicio disponibles.
         # Itera sobre todas las ubicaciones de inicio
         for i_location in self.I_locations:
             # Verifica si ya hay suficientes agentes
@@ -103,24 +106,28 @@ class CityModel(Model):
 
     def step(self):
         '''Advance the model by one step.'''
+         # Actualizar todos los agentes y realizar acciones según el estado actual del modelo.
         self.schedule.step()
         print ("STEP: ", self.schedule.steps)
         print ("ARRIVED CARS: ", self.arrived_cars)
         self.step_count += 1 
 
+        # Inicializar más coches cada ciertos pasos.
         if self.step_count % 3 == 0:
-            self.initialize_cars()
+            self.initializeCars()
         
         # if self.schedule.steps % 100 == 0:
         #     print ("STEP: ", self.schedule.steps)
         #     post(self.arrived_cars)
         #     print("POSTED NUMBER OF CARS")
-            
+        
+        # Detener la ejecución después de una cantidad específica de pasos.    
         if self.step_count == 1000 or self.schedule.steps == 1000:
             self.running = False
         
     
     def get_agent_data(self):
+        # Obtener y devolver los datos de los agentes tipo coche.
         agent_data = []
         for agent in self.schedule.agents:
             if isinstance(agent, Car):  # Puedes ajustar esto según los tipos de agentes que tengas
@@ -128,12 +135,12 @@ class CityModel(Model):
                     "id": agent.unique_id,
                     "x": agent.pos[0],
                     "y": agent.pos[1],
-                    # Añade aquí más atributos si es necesario
                 }
                 agent_data.append(agent_info)
         return agent_data
     
     def get_traffic_light_data(self):
+        # Obtener y devolver los datos de los semáforos.
         traffic_light_data = []
         for agent in self.schedule.agents:
             if isinstance(agent, TrafficLight):
@@ -150,6 +157,7 @@ class CityModel(Model):
         return traffic_light_data
 
 def post(arrived_cars):
+    # Enviar datos (número de coches llegados) a un servidor remoto.
     url = "http://52.1.3.19:8585/api/"
     endpoint = "attempts"
 
