0121c2d95caa5d56fdfd928fb416bf4c9acd71c0
Fernanda Cantú <fercantu56@gmail.com>
2023-12-02 10:08:57 -0600

    comment out code porque habian cosas duplicadas

--- StreetSimulation/Assets/Scripts/trafficLights.cs
+++ StreetSimulation/Assets/Scripts/trafficLights.cs
@@ -1,38 +1,38 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class TrafficLightController : MonoBehaviour
-{
-    [SerializeField] public bool isGreen = false;
-    public GameObject greenLight;
-    public GameObject redLight;
+// public class TrafficLightController : MonoBehaviour
+// {
+//     [SerializeField] public bool isGreen = false;
+//     public GameObject greenLight;
+//     public GameObject redLight;
 
-    public float lightChangeInterval = 5.0f; // Time interval for light change in seconds
-    private float timer;
+//     public float lightChangeInterval = 5.0f; // Time interval for light change in seconds
+//     private float timer;
 
-    void Start()
-    {
-        // Ensure the initial state of lights
-        ToggleLights();
-        timer = lightChangeInterval; // Start the timer
-    }
+//     void Start()
+//     {
+//         // Ensure the initial state of lights
+//         ToggleLights();
+//         timer = lightChangeInterval; // Start the timer
+//     }
 
-    void Update()
-    {
-        ToggleLights();
-    }
+//     void Update()
+//     {
+//         ToggleLights();
+//     }
 
-    void ToggleLights()
-    {
-        // Activate/deactivate lights based on the boolean value
-        if (isGreen)
-        {
-            greenLight.SetActive(true);
-            redLight.SetActive(false);
-        }
-        else
-        {
-            greenLight.SetActive(false);
-            redLight.SetActive(true);
-        }
-    }
-}
+//     void ToggleLights()
+//     {
+//         // Activate/deactivate lights based on the boolean value
+//         if (isGreen)
+//         {
+//             greenLight.SetActive(true);
+//             redLight.SetActive(false);
+//         }
+//         else
+//         {
+//             greenLight.SetActive(false);
+//             redLight.SetActive(true);
+//         }
+//     }
+// }
